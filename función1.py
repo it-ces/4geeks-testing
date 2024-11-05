0e99092eb67c223642a02a938a2e450105614fbc
Andres Cabrales <cabrales.andres@gmail.com>
2024-11-05 01:28:35 +0000

    Cambio 2

--- función1.py
+++ función1.py
@@ -5,7 +5,7 @@
 # fernanda la funcón tal nbecvesuta retonrar un etnero
 
 ### Agregando función 2 para la pool de Ips....
-## Segunda linea de modificacion
+## Segunda linea de modificacion, prueba de andres
 
 
 # Bacano!.... all ok.
